ee57f6e2577b2d5a43520676ef5cff3083930100
Margaret <mpbeck@umich.edu>
2019-06-18 19:38:31 -0400

    almost to MVP

--- js/index.js
+++ js/index.js
@@ -12,10 +12,9 @@ function mouseOut() {
     document.getElementById("intro").style.color = "black";
 }
 
-
 //* [ ] `keydown`
-
-
+document.addEventListener("keydown", event => {
+  alert("error cannot enter text.")})
 
 
 //* [ ] `wheel`
@@ -27,16 +26,36 @@ function myFunction() {
 }
 
 //* [ ] `drag / drop`
+function dragStart(event) {
+  event.dataTransfer.setData("document", event.target.id);
+}
+
+function dragging(event) {
+  document.getElementById("img").innerHTML = "The image cannot be copied";
+}
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("document");
+  event.target.appendChild(document.getElementById(data));
+  document.getElementById("img").innerHTML = "Images are copyrighted";
+}
 
 
 //* [ ] `load`
 
-function myFunction() {
-  alert("Page is loaded");
-}
+document.addEventListener("load", event => {
+  alert("page is loaded.")})
+
 
 //* [ ] `focus`
 
+img-content.addEventListener("focus", event => {
+  alert(:"focused image is copyrighted")})
 
 //* [ ] `resize`
 window.addEventListener('resize', (event) => alert('Page resized.'));
@@ -44,9 +63,12 @@ window.addEventListener('resize', (event) => alert('Page resized.'));
 
 //* [ ] `scroll`
 function myFunction() {
-  document.getElementById("demo").innerHTML = x += 1;
+  document.getElementById("document").innerHTML = x += 1;
 }
 //* [ ] `select`
+nav.addEventListener('select', (event) => {
+  nav.classList.toggle("makeRed");
+});
 
 //* [ ] `dblclick`
 
@@ -61,3 +83,4 @@ function myFunction() {
 
 //* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
 //* [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+nav.forEach((ele) => ele.addEventListener('click', (ev) => {ev.preventDefault()}));
