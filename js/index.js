28c8c85ceb8db551588fc78dc23da72dfc0eb7aa
Margaret <mpbeck@umich.edu>
2019-06-18 17:17:25 -0400

    java part 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
 // Your code goes here
+//`mouseover`
+
+document.getElementById("intro").onmouseover = function() {mouseOver()};
+document.getElementById("intro").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+    document.getElementById("intro").style.color = "lightblue";
+}
+
+function mouseOut() {
+    document.getElementById("intro").style.color = "black";
+}
+
+
+//* [ ] `keydown`
+
+
+
+
+//* [ ] `wheel`
+
+document.getElementById("explanantion").addEventListener("wheel", myFunction);
+
+function myFunction() {
+  this.style.fontSize = "39px";
+}
+
+//* [ ] `drag / drop`
+
+
+//* [ ] `load`
+
+
+//* [ ] `focus`
+
+
+//* [ ] `resize`
+
+
+//* [ ] `scroll`
+//* [ ] `select`
+
+//* [ ] `dblclick`
+
+document.getElementById("intro").addEventListener("dblclick", myFunction);
+
+function myFunction() {
+  document.getElementById("intro").innerHTML = "I was double-clicked!";
+}
+
+
+
+
+//* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+//* [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
